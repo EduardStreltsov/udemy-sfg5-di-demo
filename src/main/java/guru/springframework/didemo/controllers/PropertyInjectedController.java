b26e716f25c09af31fa22cb68b9719e43c434916
eduard.streltsov <streltsov.eduard@gmail.com>
2018-08-02 20:24:55 +0300

    Initial commit

---
+++ src/main/java/guru/springframework/didemo/controllers/PropertyInjectedController.java
@@ -0,0 +1,17 @@
+package guru.springframework.didemo.controllers;
+
+import guru.springframework.didemo.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PropertyInjectedController {
+	
+	@Autowired
+	public GreetingService greetingService;
+	
+	String sayGreeting(){
+		return greetingService.sayGreeting();
+	}
+	
+}
---
+++ src/main/java/guru/springframework/didemo/controllers/SetterInjectedController.java
@@ -0,0 +1,20 @@
+package guru.springframework.didemo.controllers;
+
+import guru.springframework.didemo.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SetterInjectedController {
+	
+	private GreetingService greetingService;
+	
+	public String sayGreeting() {
+		return greetingService.sayGreeting();
+	}
+	
+	@Autowired
+	public void setGreetingService(GreetingService greetingService) {
+		this.greetingService = greetingService;
+	}
+}
