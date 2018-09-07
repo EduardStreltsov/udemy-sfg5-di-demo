dfbb18ef329522b8a3edf2e7b86ec416939b9407
Eduard Streltsov <streltsov.eduard@gmail.com>
2018-09-07 10:13:13 +0300

    Lecture 82

---
+++ src/main/java/guru/springframework/didemo/configs/GreetingServiceConfig.java
@@ -0,0 +1,9 @@
+package guru.springframework.didemo.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+
+@Configuration
+public class GreetingServiceConfig {
+	
+}
